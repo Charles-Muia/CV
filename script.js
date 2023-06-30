6b1c7b6189d8e68ec4c24d7f8d2019a4683b3984
Charles-Muia <Charles.K.Muia@gmail.com>
2023-06-30 14:00:10 +0300

    My_CV

--- script.js
+++ script.js
@@ -16,3 +16,12 @@
         menu.classList.remove('bx-menu');
         navlinks.classList.remove('active');
     };
+    
+   const sr = ScrollReveal ({
+       distance: '45px',
+       duration: 2700,
+       reset: true
+   })
+
+    sr.reveal ('.home-sec-text',{delay:350, origin:'left' })
+    sr.reveal ('.home-sec-img',{delay:350, origin:'right' })
