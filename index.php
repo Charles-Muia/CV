bac0bd51b82cb50c45d06a542c55206efc270e06
Charles-Muia <Charles.K.Muia@gmail.com>
2023-09-07 17:52:04 +0300

    My_CV

--- index.php
+++ index.php
@@ -20,8 +20,10 @@
 
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<!----Google Fonts----------------------------------------------------------------------------------------------------->
+
+    <!----Google Fonts----------------------------------------------------------------------------------------------------->
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -43,7 +45,7 @@
             <li><a href="#services">services</a></li>
             <li><a href="#contact">contact</a></li>
         </ul>
-            <a href="#" class="top-btn">download my CV</a>
+            <a download="Gallery/Resume.pdf" href="Gallery/Resume.pdf" class="top-btn"> <i class="fa fa-download"></i> my CV </a>
 </header>
 
 <!----HomeSection------------------------------------------------------------------------------------------------------>
