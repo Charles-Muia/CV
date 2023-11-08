fadae225859e36b0acd30b3be154880a93062900
Charles-Muia <Charles.K.Muia@gmail.com>
2023-11-08 15:34:27 +0300

    My_CV

--- form-handler.php
+++ form-handler.php
@@ -1,6 +1,29 @@
 <?php
+
+if (isset($_POST['submit'])) {
+
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
+
+    $email_from = 'info@karlmagne.com';
+
+    $email_subject = 'New Alert: You have received an e-mail.';
+
+    $email_body =   "Sender's Name: $name.\n".
+                    "Sender's Email: $email.\n".
+                    "Subject: $message.\n";
+
+    $to = 'info.techwarescopesolutions@gmail.com';
+
+    $header = "From: $email_from \r\n";
+
+    $header .= "Reply-To: $email \r\n";
+
+    mail($to,$email_subject,$email_body,$header);
+
+    header ("Location: index.php");
+
+    }
 
 ?>
--- index.php
+++ index.php
@@ -278,22 +278,26 @@
 
                     <form action="form-handler.php" method="post">
                             <div class="form-input">
-                                <input type="text" placeholder="Enter Your Name" name="name">
-                            </div>
+                                    <input type="text" placeholder="Enter Your Name" name="name" id="contact-name" onkeyup="validateName()">
+                                        <span id="name-error"></span>
+                                    </div>
                                     <div class="form-input">
-                                    <input type="text" placeholder="Enter Your Email Address" name="email">
+                                    <input type="text" placeholder="Enter Your Email Address" name="email" id="contact-email" onkeyup="validateEmail()">
+                                        <span id="email-error"></span>
                                     </div>
                                     <div class="form-input message-box">
-                                    <textarea placeholder="Type Your Message Here!" name="message"></textarea>
+                                    <textarea placeholder="Type Your Message Here!" name="message" id="contact-message" onkeyup="validateMessage()"></textarea>
+                                        <span id="message-error"></span>
                                     </div>
                             <div class="form-button">
-                            <input type="button" value="Send Message">
+                            <input type="button" value="Send Message" name="submit" onclick="return validateForm()">
+                                <span id="submit-error"></span>
                             </div>
                     </form>
                   </div>
                 </div>
-            </div>
 </section>
+
 
 <!----FooterSection---------------------------------------------------------------------------------------------------->
 
@@ -399,5 +403,69 @@
             };
 
         </script>
+
+<!--===ERROR-MENU----------------------------------------------------------------------------------------------------->
+
+    <script>
+        var nameError = document.getElementById('name-error');
+        var emailError = document.getElementById('email-error');
+        var messageError = document.getElementById('message-error');
+        var submitError = document.getElementById('submit-error')
+
+        function validateName() {
+            var name = document.getElementById('contact-name').value;
+
+            if(name.length == 0) {
+                nameError.innerHTML = '*Name Field is Empty!';
+                return false;
+            }
+
+            if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+                nameError.innerHTML = 'Enter Your Full Names';
+                return false;
+            }
+            nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+            return true;
+        }
+
+        function validateEmail() {
+            var email = document.getElementById('contact-email').value;
+
+            if(email.length == 0){
+                emailError.innerHTML = '*Email Address Field is Empty!';
+                return false;
+            }
+
+            if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+                emailError.innerHTML = 'Enter Your Valid Email Address';
+                return false;
+            }
+            emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+            return true;
+        }
+
+        function validateMessage() {
+            var message = document.getElementById('contact-message').value;
+            var required = 1;
+            var left = required - message.length;
+
+            if(left > 0) {
+                messageError.innerHTML = '*Message Field is Empty!';
+                return false;
+            }
+            messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+            return true;
+        }
+
+        function validateForm() {
+            if(!validateName() || !validateEmail() || !validateMessage()) {
+                submitError.style.display = 'block';
+                submitError.innerHTML = 'Empty Fields! *Required';
+                setTimeout(function (){submitError.style.display = 'none';}, 3500);
+                return false;
+            }
+        }
+    </script>
+
 </body>
 </html>
